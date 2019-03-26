a9cf92715849a062f6d08d646989027bafa7f0a4
Gupta <amrita.gupta@capgemini.com>
2019-03-26 12:41:01 +0530

    first commit

---
+++ app3.js
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+var server = app.listen(3000, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+})
+'use strict';
+
+var util = require('util');
+//var async = require('async');
+var msRestAzure = require('ms-rest-azure');
+var ComputeManagementClient = require('azure-arm-compute');
+var StorageManagementClient = require('azure-arm-storage');
+var NetworkManagementClient = require('azure-arm-network');
+var ResourceManagementClient = require('azure-arm-resource').ResourceManagementClient;
+
+
+_validateEnvironmentVariables();
+var clientId = process.env['CLIENT_ID'];
+var domain = process.env['DOMAIN'];
+var secret = process.env['APPLICATION_SECRET'];
+var subscriptionId = process.env['AZURE_SUBSCRIPTION_ID'];
+console.log(clientId);
+console.log(secret);
+var resourceClient, computeClient, storageClient, networkClient;
+//Sample Config
+var randomIds = {};
+var location = 'westus';
+var accType = 'Standard_LRS';
+
+// Ubuntu config
+var publisher = 'Canonical';
+var offer = 'UbuntuServer';
+var sku = '14.04.3-LTS';
+var osType = 'Linux';
+
+// Windows config
+//var publisher = 'microsoftwindowsserver';
+//var offer = 'windowsserver';
+//var sku = '2012-r2-datacenter';
+//var osType = 'Windows';
+
+var adminUsername = 'notadmin';
+var adminPassword = 'Pa$$w0rd92';
+
+///////////////////////////////////////////
+//     Entrypoint for sample script      //
+///////////////////////////////////////////
+app.post('/azure', function (req, res) {
+
+    msRestAzure.loginWithServicePrincipalSecret(clientId, secret, domain, function (err, credentials, subscriptions) {
+        if (err) return console.log(err);
+        //console.log(credentials)
+        resourceClient = new ResourceManagementClient(credentials, subscriptionId);
+        computeClient = new ComputeManagementClient(credentials, subscriptionId);
+        storageClient = new StorageManagementClient(credentials, subscriptionId);
+        networkClient = new NetworkManagementClient(credentials, subscriptionId);
+        switch (req.body.queryResult.intent.displayName) {
+            case "createresourceonazure":
+                var getResourceNamee = req.body.queryResult.parameters.resourcename;
+                var resourceGroupName = getResourceNamee.toString();
+                createResourceGroup(resourceGroupName, function (err, result) {
+                    if (err) {
+                        console.log("error in creating resource acocount ");
+                    } else {
+                        console.log("Resource group is created successfully");
+                    }
+                });
+            break;
+        }
+    });
+
+});
+
+function createResourceGroup(resourceGroupName, callback) {
+    var groupParameters = { location: location, tags: { sampletag: 'sampleValue' } };
+    console.log('\n1.Creating resource group: ' + resourceGroupName);
+    return resourceClient.resourceGroups.createOrUpdate(resourceGroupName, groupParameters, callback);
+}
+function _validateEnvironmentVariables() {
+    var envs = [];
+    if (!process.env['CLIENT_ID']) envs.push('CLIENT_ID');
+    if (!process.env['DOMAIN']) envs.push('DOMAIN');
+    if (!process.env['APPLICATION_SECRET']) envs.push('APPLICATION_SECRET');
+    if (!process.env['AZURE_SUBSCRIPTION_ID']) envs.push('AZURE_SUBSCRIPTION_ID');
+    if (envs.length > 0) {
+        throw new Error(util.format('please set/export the following environment variables: %s', envs.toString()));
+    }
+}
+
+function _generateRandomId(prefix, exsitIds) {
+    var newNumber;
+    while (true) {
+        newNumber = prefix + Math.floor(Math.random() * 10000);
+        if (!exsitIds || !(newNumber in exsitIds)) {
+            break;
+        }
+    }
+    return newNumber;
+}
